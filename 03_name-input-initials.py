da4fdbb2c6464bd947ce98fc744119b36c086a6b
Ya Sin Allah <yasin.allahdev@gmail.com>
2020-04-01 20:22:25 -0400

    Excersize 3: Initials Generator

---
+++ 03_name-input-initials.py
@@ -0,0 +1,4 @@
+firstName = input("Enter your first name: ")
+middleName = input("Enter your middle name: ")
+lastName = input("Enter your past name: ")
+print("Your initials are",firstName[0],middleName[0],lastName[0])
