b3f8f9fd317a4a771be036e629fb15e902ba7c2e
Ya Sin Allah <yasin.allahdev@gmail.com>
2020-04-03 11:27:34 -0400

    Numbers Excersize: Area & Circumfrence of a circle

---
+++ 05_area-and-circumfrence-of-circle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Enter a number to be the radius of the circle: "))
+print("The area of the circle is: ", round(math.pi * (radius ** 2), 2))
+print("The circumfrence of the circle is: ", round(2 * math.pi * radius, 2))
